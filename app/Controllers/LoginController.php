535aa3acb6fd953291286b3db19305ee64dce498
Haryo Bimantoro <102338707+Haryo2021@users.noreply.github.com>
2023-10-02 23:44:27 +0700

    Add files via upload

---
+++ app/Controllers/LoginController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\Mlogin;
+use App\Models\Mmember;
+
+class LoginController extends RestfulController
+{
+    
+    public function login()
+    {
+        $email = $this -> request -> getVar('email');
+        $password = $this -> request -> getVar('password');
+
+        $model = new Mmember();
+        $member = $model -> where(['email' => $email]) -> first();
+        if (!$member){
+            return $this -> responseHasil (400, false, "Email tidak ditemukan");
+        }
+        if (!password_verify($password, $member['password'])){
+            return $this ->responseHasil(400, false, "Password tidak valid");
+        }
+
+        $login = new Mlogin();
+        $auth_key = $this -> RandomString();
+        $login -> save([
+            'member_id' => $member['id'],
+            'auth_key' => $auth_key
+        ]);
+        $data = [
+            'token' => $auth_key,
+            'user' => [
+                'id' => $member['id'],
+                'email' => $member['email'],
+            ]
+        ];
+            return $this -> responseHasil(200, true, $data);
+    }
+
+    private fuction RandomString($length = 100)
+    {
+        $karakter = '012345678dssd9abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $panjang_karakter = strlen($karakter);
+        $str = '';
+        for ($i = 0; $i <$length; $i++){
+            $str .= $karakter[rand(0, $panjang_karakter - 1)];
+        }
+        return $str;
+    }
+}
+?>
